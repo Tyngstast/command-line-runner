57a27fa31ec735a2054ea3b81dc8da5768cb341c
Henrik Johansson <henrj@redpill-linpro.com>
2019-02-23 18:29:43 +0100

    fixed abort message for execute command not found

--- app/rest/commands.py
+++ app/rest/commands.py
@@ -63,7 +63,7 @@ class CommandExecuteResource(Resource):
         command = Command.query.filter_by(id=id).first()
 
         if not command:
-            return abort(404, 'Command not found')
+            return abort(404, message='Command not found')
 
         args = shlex.split(command.value)
 
