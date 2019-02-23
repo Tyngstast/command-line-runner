2e2629cdea70b8103637cd2d4b61dcd19dca23f5
Henrik Johansson <henrj@redpill-linpro.com>
2019-02-23 19:03:47 +0100

    Added support for python3 venv activation in run.sh. Fixed so database initialization and run.sh is not dependent on relative path.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-DB_FILE = 'app.db'
+DB_FILE = os.path.join(basedir, 'app.db')
 
 # DB file
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, DB_FILE)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DB_FILE
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Bundle reqparse errors 
--- run.sh
+++ run.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:./ 
+DIR=$(dirname "$0")
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    VENV=$DIR/venv/bin/activate
+    if [ ! -f "$VENV" ]; then
+        VENV=$HOME/.pyenv/versions/3.5.3/envs/commandlinerunner/bin/activate
+    fi
+    echo "VIRTUAL_ENV not activated, activating..."
+    source $VENV 
+fi
+
+export PYTHONPATH=$PYTHONPATH:$DIR/
 
 export FLASK_APP=app
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=5000
+
 flask run
