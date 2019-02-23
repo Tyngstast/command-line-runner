ba379c9a73fa0c108697df3d238c3a41195d2656
Henrik Johansson <henrj@redpill-linpro.com>
2019-02-23 19:12:24 +0100

    Cleaned up run script

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 DIR=$(dirname "$0")
+PYTHON_VERSION=3.5.3
 
 if [ -z "$VIRTUAL_ENV" ]; then
-    VENV=$DIR/venv/bin/activate
-    if [ ! -f "$VENV" ]; then
-        VENV=$HOME/.pyenv/versions/3.5.3/envs/commandlinerunner/bin/activate
+    VENV=$DIR/venv
+    if [ ! -d "$VENV" ]; then
+        VENV=$HOME/.pyenv/versions/$PYTHON_VERSION/envs/commandlinerunner
+    fi
+    if [ ! -d "$VENV" ]; then
+        echo "virtual environment not found, exiting..." 
+        return
     fi
     echo "VIRTUAL_ENV not activated, activating..."
-    source $VENV 
+    source $VENV/bin/activate 
 fi
 
 export PYTHONPATH=$PYTHONPATH:$DIR/
@@ -17,5 +22,7 @@ export FLASK_APP=app
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=5000
+
+echo "Starting flask app..."
 
 flask run
